5b67b3444ef3f333f6bebfd6d092491c8437d2da
Hekang <39700571+JustKeepSilence@users.noreply.github.com>
2021-02-01 20:15:16 +0800

    Delete app directory

--- app/README.md
+++
@@ -1,3 +0,0 @@
-# app
-
-> Web application based on vue
--- app/src/mobile/index.js
+++
@@ -1,8 +0,0 @@
-// window.onresize = setHtmlFontSize
-// function setHtmlFontSize(){
-//     const htmlWidth = document.documentElement.clientWidth || document.body.clientWidth
-//     const htmlDom = document.getElementsByTagName('html')[0]
-//     htmlDom.style.fontSize = htmlWidth / 10 + 'px'
-// }
-
-// setHtmlFontSize()
