be5eccdd9e9a6b749b4997fb055c0321ad7471e5
Hekang <39700571+JustKeepSilence@users.noreply.github.com>
2021-03-22 16:51:06 +0800

    remove authorization.go to middleware.go

--- db/authorization.go
+++ db/authorization.go
@@ -6,28 +6,3 @@ goVersion: 1.15.3
 */
 
 package db
-
-import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-)
-
-// authorization middleware, for details see:
-func (gdb *Gdb) authorization() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// for userLogin not need authorization
-		if c.Request.URL.String() != "/page/userLogin" {
-			if userName, token, ok := c.Request.BasicAuth(); !ok {
-				c.AbortWithStatus(401)
-			} else {
-				if v, err := gdb.infoDb.Get([]byte(userName+"_token"), nil); err != nil || v == nil {
-					c.AbortWithStatus(401)
-				} else {
-					if token != fmt.Sprintf("%s", v) {
-						c.AbortWithStatus(401)
-					}
-				}
-			}
-		}
-	}
-}
