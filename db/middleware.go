1b62c6356c62602bf5143367cc43e146b7a7eaba
Hekang <39700571+JustKeepSilence@users.noreply.github.com>
2021-03-24 14:46:37 +0800

    add logs for restful service of gdb

--- db/middleware.go
+++ db/middleware.go
@@ -12,6 +12,7 @@ package db
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
 
 // authorization middleware
@@ -34,7 +35,45 @@ func (gdb *Gdb) authorization() gin.HandlerFunc {
 	}
 }
 
-// log middleware
-func (gdb *Gdb) logsWriting() gin.HandlerFunc {
-	return nil
+// writing logs and response body
+
+func (gdb *Gdb) string(c *gin.Context, code int, formatter string, responseData []byte) {
+	if level, ok := c.Request.Header["logLevel"]; ok {
+		if level[0] == "0" {
+			// info level
+			if code == 200 {
+				if c.Request.Method == "POST" {
+					b, _ := ioutil.ReadAll(c.Request.Body)
+					_ = gdb.writeLog(Info, c.Request.URL.String(), fmt.Sprintf("%s", b), "POST", "", c.Request.RemoteAddr)
+				} else {
+					_ = gdb.writeLog(Info, c.Request.URL.String(), c.Request.URL.String(), "GET", "", c.Request.RemoteAddr)
+				}
+			} else {
+				if c.Request.Method == "POST" {
+					b, _ := ioutil.ReadAll(c.Request.Body)
+					_ = gdb.writeLog(Error, c.Request.URL.String(), fmt.Sprintf("%s", b), "POST", fmt.Sprintf("%s", responseData), c.Request.RemoteAddr)
+				} else {
+					_ = gdb.writeLog(Error, c.Request.URL.String(), c.Request.URL.String(), "GET", fmt.Sprintf("%s", responseData), c.Request.RemoteAddr)
+				}
+			}
+		} else {
+			// error
+			if code != 200 {
+				if c.Request.Method == "POST" {
+					b, _ := ioutil.ReadAll(c.Request.Body)
+					_ = gdb.writeLog(Error, c.Request.URL.String(), fmt.Sprintf("%s", b), "POST", fmt.Sprintf("%s", responseData), c.Request.RemoteAddr)
+				} else {
+					_ = gdb.writeLog(Error, c.Request.URL.String(), c.Request.URL.String(), "GET", fmt.Sprintf("%s", responseData), c.Request.RemoteAddr)
+				}
+			}
+		}
+	}
+	c.String(code, formatter, responseData)
+}
+
+// set logType to request headers
+func (gdb *Gdb) setLogHeader(level logLevel) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Request.Header.Add("logLevel", string(rune(level)))
+	}
 }
