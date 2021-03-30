50efc5b0cd7a249eca231806a4a202d919e79f7d
JustKeepSilence <hekang0426@gmail.com>
2021-03-30 16:05:57 +0800

    update main function

--- main/main.go
+++ main/main.go
@@ -14,9 +14,15 @@ func main() {
 	if !os.IsExist(err) {
 		_ = os.MkdirAll("./uploadFiles", 0766)
 	}
-	if err := db.InitialDbServer(); err != nil {
-		log.Print(fmt.Errorf("System initialization failed: " + err.Error()))
+	if configs, err := db.ReadDbConfig("./config.json"); err != nil {
+		log.Println(err)
 		time.Sleep(60 * time.Second)
 		return
+	} else {
+		if err := db.StartDbServer(configs); err != nil {
+			log.Println(fmt.Errorf("System initialization failed: " + err.Error()))
+			time.Sleep(60 * time.Second)
+			return
+		}
 	}
 }
