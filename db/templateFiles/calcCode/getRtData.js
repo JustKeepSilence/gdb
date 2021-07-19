cac527f117186b7beeac7760505a39c9bea96af4
Hekang <39700571+JustKeepSilence@users.noreply.github.com>
2021-07-19 13:11:06 +0800

    Delete db/templateFiles/calcCode directory

--- db/templateFiles/calcCode/getRtData.js
+++
@@ -1,8 +0,0 @@
-// getRtData(itemNames: []string{}): 获取gdb中的实时值
-// 输入参数：要获取的item
-var r = JSON.parse(getRtData(["t1", "t2"], ["calc", "calc"]))  // 获取点的实时值,返回的数据类型均为string: {"t1": v1, "t2": v2}
-if (r["t1"] !== null && r["t2"] !== null){
-    // 一定要判断取到的数值是否为空来避免向数据库写入NaN
-    var x = (r["t1"] + r["t2"]) / 2
-    writeRtData([{"itemName": "x", "value": x, "groupName": "calc"}])  // 写入单点的实时值
-}
--- db/templateFiles/calcCode/mock.js
+++
@@ -1,5 +0,0 @@
-// mock 
-
-var t1 = Math.random()
-var t2 = Math.random() * 1.5
-writeRtData([{"itemName": "t1", "value": t1, "groupName": "calc"},{"itemName": "t2", "value": t2, "groupName": "calc"}])  
